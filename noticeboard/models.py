2e63ad9bbf3dcb7fece444ef3b55c44d19a66e3e
kang <slash@zigbang.com>
2018-04-05 14:25:35 +0900

    장고 프로젝트

---
+++ noticeboard/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Feedback(models.Model):
+
+    Gender = (
+        ('남성', 'man'),
+        ('여성', 'woman'),
+    )
+    title = models.CharField(max_length=200)
+    gender = models.CharField(max_length=2, choices=Gender)
+    name = models.CharField(max_length=10)
+    age = models.IntegerField()
+    email = models.EmailField()
