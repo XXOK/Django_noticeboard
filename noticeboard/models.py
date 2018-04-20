0a57dda0cbd78ad33287494e662769b4fbd6acd4
kang <slash@zigbang.com>
2018-04-20 12:43:53 +0900

    유튜브 영상 삽입, 댓글 남기기

--- noticeboard/models.py
+++ noticeboard/models.py
@@ -7,7 +7,7 @@ class Feedback(models.Model):
         ('남성', 'man'),
         ('여성', 'woman'),
     )
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=1000)
     gender = models.CharField(max_length=2, choices=Gender)
     name = models.CharField(max_length=10)
     age = models.IntegerField()
